ccd482a438481447bfedf36dc6e56b61e9e280e3
Irungu Macharia <machariajosphat001@gmail.com>
2023-07-28 17:17:56 +0000

    cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,6 +5,10 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
     """implements put and get methods"""
+    def __init__(self) -> None:
+        """BaseCaching Caching System"""
+        super().__init__()
+
     def put(self, key, item):
         """puts a value"""
         if key is None or item is None:
@@ -13,6 +17,6 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """gets a value"""
-        if key is None or (key not in self.cache_data):
+        if key is None or self.cache_data.get(key) is None:
             return None
-        return self.cache_data[key]
+        return self.cache_data.get(key)
